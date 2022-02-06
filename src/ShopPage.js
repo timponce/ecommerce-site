3c350aa6d5734e6853353ae4bf81d9d275cbe468
Timothy Ponce <timothyjponce@gmail.com>
2022-02-05 17:57:47 -0800

    Add styling to product grid

--- src/ShopPage.js
+++ src/ShopPage.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import ProductGrid from "./components/ProductGrid";
 import Footer from "./components/Footer";
 
 export default function ShopPage(props) {
   return (
     <div>
       <Navbar />
+      <div className="product-grid-container">
+        <ProductGrid selectedProducts={props.products.hairTiesArray} />
+        <ProductGrid selectedProducts={props.products.hairProductsArray} />
+        <ProductGrid selectedProducts={props.products.hairBrushesArray} />
+      </div>
       <Footer />
     </div>
   );
--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -1,5 +1,21 @@
 import React from "react";
 
-export default function ProductGrid() {
-  return <div className="product-grid">Hello</div>;
+export default function ProductGrid(props) {
+  const productCards = props.selectedProducts.map((item) => (
+    <div className="product-card" key={item.id}>
+      <h1 className="product-card--placeholder-text">{item.name}</h1>
+      <button className="product-card--button">
+        <span>Add to Bag</span> <span>{item.price}</span>
+      </button>
+    </div>
+  ));
+
+  return (
+    <div className="product-grid--section">
+      <h1 className="product-grid--heading">
+        {props.selectedProducts[0].category}
+      </h1>
+      <div className="product-grid">{productCards}</div>
+    </div>
+  );
 }
